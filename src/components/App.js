0aabe7d0b00b8828bac8c9553598102ed12d31c0
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-12-18 13:23:08 +0000

    n

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [a,fun]=useState(0);
+  function increase(){
+    fun(a+1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button Cliked {a} times</p>
+        <button onClick={increase}>Click me</button>
     </div>
   )
 }
